5ecbb514887c331a3d0c8283e38c8188d8a2ca95
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-09 13:09:30 +0530

    pattern 1

---
+++ DSA/1. Pattern/1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// sq number pattern
+int main(){
+    for(int i =0;i<4;i++){
+        for(int j =1;j<=4;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
