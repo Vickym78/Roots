35796f58e85af3f24828b8a022fae6f17a43ec9a
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-09 13:16:42 +0530

    sqaure patterns

--- DSA/1. Pattern/1.cpp
+++ DSA/1. Pattern/1.cpp
@@ -9,4 +9,49 @@ int main(){
         }
         cout<<endl;
     }
+     cout<<endl<<"---charcters---"<<endl;
+    // now for character
+    for(int i =1;i<=4;i++){
+        char ch = 'A';
+        for(int j =0;j<4;j++){
+            cout<<char(ch+j)<<" ";
+
+        }
+        cout<<endl;
+    }
+
+
+    cout<<endl<<"--continous number--"<<endl;
+    int a = 1;
+    for(int i =0;i<3;i++){
+        for(int  j  =0;j<3;j++){
+        cout<<a<<" ";
+        a++;
+        }
+        cout<<endl;
+    }
+
+    /*
+    
+   Expected output
+
+1 2 3 4 
+1 2 3 4
+1 2 3 4
+1 2 3 4
+
+---charcters---
+A B C D
+A B C D
+A B C D
+A B C D
+
+--continous number--
+1 2 3
+4 5 6
+7 8 9
+    
+    
+    */
+    
 }
