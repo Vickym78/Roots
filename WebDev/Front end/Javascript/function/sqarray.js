84bc685cfc46aa71f6873661e3ebe30946a76f1c
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-13 22:11:47 +0530

    Function eg - 2

--- WebDev/Front end/Javascript/function/sqarray.js
+++ WebDev/Front end/Javascript/function/sqarray.js
@@ -3,4 +3,6 @@ let arr=[1,2,3,4,5]
 arr.forEach((val, idx)=>{
     console.log(val*val, idx);
     
-}) // for Each is HOF takiing argument as function
+}) 
+
+// for Each is HOF takiing argument as function
