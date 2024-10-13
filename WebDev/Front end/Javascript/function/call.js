38f5629a739a5a59b4d89fef19c8dbe5fc3c8d9c
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-13 12:41:26 +0530

    Function eg - 1

--- WebDev/Front end/Javascript/function/call.js
+++ WebDev/Front end/Javascript/function/call.js
@@ -1,7 +1,15 @@
-function sum(x,y){
+/*function sum(x,y){
     s=x+y;
     console.log(s);
     
 }
 
 sum(5,3)
+*/
+// arrow fuinction
+const sm = (a,b)=>{
+    console.log(a+b);
+    
+}
+
+sm(5,6);
