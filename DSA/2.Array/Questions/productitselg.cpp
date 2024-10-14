28d653c5e81c532bf2df1a58c2226eade2864a68
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-14 14:55:34 +0530

    array product except itself

--- DSA/2.Array/Questions/productitselg.cpp
+++ DSA/2.Array/Questions/productitselg.cpp
@@ -1,8 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void arrayproduct(vector<int> &arr){
-    
+void arrayproduct(vector<int> &arr  ,int n){
+    vector<int>ans(n);
+    vector<int>pre(n,1);
+    vector<int>suf(n,1);
+    for(int i = 1; i<n;i++){
+        pre[i] = pre[i-1]*arr[i-1];
+    }
+
+    for(int i = n-2; i>=0;i--){
+            suf[i] = suf[i+1] * arr[i+1];
+    }
+
+    for(int i = 0 ; i<n; i++){
+        ans[i] = pre[i]*suf[i];
+    }
+
+    for(int i = 0 ; i<n ; i++){
+    cout<<ans[i]<<" " ;
+        }
 }
 
 int main(){
@@ -12,5 +29,5 @@ int main(){
         cin>>v[i];
     }
 
-    arrayproduct(v);
+    arrayproduct(v , n);
 }
