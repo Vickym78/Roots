6d09ee8e9d6d94da147430393768bf7b516fbd33
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-14 14:51:05 +0530

    array product except itself

---
+++ DSA/2.Array/Questions/productitselg.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void arrayproduct(vector<int> &arr){
+    
+}
+
+int main(){
+    int n; cin>>n;
+    vector<int>v(n);
+    for(int i = 0 ; i<n;i++){
+        cin>>v[i];
+    }
+
+    arrayproduct(v);
+}
