0c9642ada2ea6ce9415b3b298c1a85f4e4e505ef
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-14 19:39:50 +0530

    JS DOM

--- WebDev/Front end/Javascript/DOM/style.js
+++ WebDev/Front end/Javascript/DOM/style.js
@@ -1,2 +1,9 @@
-console.log("Hello ,How are you");
-alert("this is demo page")
+let element = document.querySelector('h2')
+console.log(element.innerText)
+element.innerText = element.innerText + " from javascript change";
+
+
+let divs = document.querySelectorAll('.box');
+divs[0].innerText = divs[0].innerText + "  ADding this extra";
+
+alert("Demo to changing through js");
