586374a94a357a230aca3351c87bad079008b447
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-16 13:18:39 +0530

    Bubble sort

---
+++ DSA/2.Array/Sorting/bubblesort.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n ; cin>>n ; 
+    vector<int>v(n);
+    for(int i = 0 ; i<n;i++){
+        cin>>v[i];
+    }
+        // bubble sort
+        for(int i =0;i<n-1;i++){
+            for(int j = i+1; j<n;j++){
+                if(v[i]>v[j]) swap(v[i],v[j]);
+            }
+        }
+
+        for(int i = 0 ; i<n;i++){
+            cout<<v[i]<<" ";
+        }
+    
+}
