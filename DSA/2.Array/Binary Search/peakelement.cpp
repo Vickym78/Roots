b9e32add14f9021ccc9549a8b7397e806130bfb5
Vicky Mahato <vickymahato2003@gmail.com>
2024-10-16 14:24:09 +0530

    Peak element

--- DSA/2.Array/Binary Search/peakelement.cpp
+++ DSA/2.Array/Binary Search/peakelement.cpp
@@ -12,8 +12,24 @@ void bylinears(vector<int> &arr){
 
 }
 
+void binarysearch(vector<int> &arr){
+    if(arr.size()<3) cout<<"not exist";
+    int lo = 0 , hi = arr.size()-1;
+    while(lo<=hi){
+        int mid = lo + (hi-lo)/2;
+        if(arr[mid]> arr[mid-1] && arr[mid]>arr[mid+1]) {cout<<arr[mid]<<" "; break;}
+
+        if(arr[mid] >arr[mid-1]){
+            lo = mid+1;
+        }
+        else hi = mid-1;
+
+    }
+}
+
 int main(){
         vector<int>arr = {0,3,8,9,5,2};
 
         bylinears(arr);
+        binarysearch(arr);
 }
